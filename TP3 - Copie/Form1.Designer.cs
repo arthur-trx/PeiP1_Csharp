f979b8e5eeaf5fddf1a3292d86e9cf51041aa467
arthur-trx <90566883+arthur-trx@users.noreply.github.com>
2022-10-18 11:24:04 +0200

    Add files via upload

---
+++ TP3 - Copie/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TP3
+{
+    partial class label_taux
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_taux = new System.Windows.Forms.TextBox();
+            this.label_devise_1 = new System.Windows.Forms.Label();
+            this.label_devise_2 = new System.Windows.Forms.Label();
+            this.radioButton_Dollars = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.label_resultat_devise2 = new System.Windows.Forms.Label();
+            this.radioButtonEuros_dollars = new System.Windows.Forms.RadioButton();
+            this.radioButtonDollars_euros = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "taux de conversion : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_taux
+            // 
+            this.textBox_taux.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_taux.Location = new System.Drawing.Point(313, 42);
+            this.textBox_taux.Name = "textBox_taux";
+            this.textBox_taux.Size = new System.Drawing.Size(125, 29);
+            this.textBox_taux.TabIndex = 1;
+            this.textBox_taux.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label_devise_1
+            // 
+            this.label_devise_1.AutoSize = true;
+            this.label_devise_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_devise_1.Location = new System.Drawing.Point(76, 129);
+            this.label_devise_1.Name = "label_devise_1";
+            this.label_devise_1.Size = new System.Drawing.Size(41, 16);
+            this.label_devise_1.TabIndex = 2;
+            this.label_devise_1.Text = "euros";
+            this.label_devise_1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label_devise_2
+            // 
+            this.label_devise_2.AutoSize = true;
+            this.label_devise_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_devise_2.Location = new System.Drawing.Point(411, 129);
+            this.label_devise_2.Name = "label_devise_2";
+            this.label_devise_2.Size = new System.Drawing.Size(48, 16);
+            this.label_devise_2.TabIndex = 3;
+            this.label_devise_2.Text = "dollars";
+            // 
+            // radioButton_Dollars
+            // 
+            this.radioButton_Dollars.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton_Dollars.Location = new System.Drawing.Point(79, 160);
+            this.radioButton_Dollars.Name = "radioButton_Dollars";
+            this.radioButton_Dollars.Size = new System.Drawing.Size(125, 29);
+            this.radioButton_Dollars.TabIndex = 4;
+            this.radioButton_Dollars.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.button.Location = new System.Drawing.Point(254, 160);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(82, 29);
+            this.button.TabIndex = 5;
+            this.button.Text = "->";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label_resultat_devise2
+            // 
+            this.label_resultat_devise2.AutoSize = true;
+            this.label_resultat_devise2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label_resultat_devise2.Location = new System.Drawing.Point(390, 160);
+            this.label_resultat_devise2.Name = "label_resultat_devise2";
+            this.label_resultat_devise2.Size = new System.Drawing.Size(65, 24);
+            this.label_resultat_devise2.TabIndex = 6;
+            this.label_resultat_devise2.Text = "dollars";
+            // 
+            // radioButtonEuros_dollars
+            // 
+            this.radioButtonEuros_dollars.AutoSize = true;
+            this.radioButtonEuros_dollars.Checked = true;
+            this.radioButtonEuros_dollars.Location = new System.Drawing.Point(79, 266);
+            this.radioButtonEuros_dollars.Name = "radioButtonEuros_dollars";
+            this.radioButtonEuros_dollars.Size = new System.Drawing.Size(99, 17);
+            this.radioButtonEuros_dollars.TabIndex = 7;
+            this.radioButtonEuros_dollars.TabStop = true;
+            this.radioButtonEuros_dollars.Text = "Euros -> Dollars";
+            this.radioButtonEuros_dollars.UseVisualStyleBackColor = true;
+            this.radioButtonEuros_dollars.UseWaitCursor = true;
+            this.radioButtonEuros_dollars.CheckedChanged += new System.EventHandler(this.radioButtonEuros_dollars_CheckedChanged);
+            // 
+            // radioButtonDollars_euros
+            // 
+            this.radioButtonDollars_euros.AutoSize = true;
+            this.radioButtonDollars_euros.Location = new System.Drawing.Point(79, 308);
+            this.radioButtonDollars_euros.Name = "radioButtonDollars_euros";
+            this.radioButtonDollars_euros.Size = new System.Drawing.Size(99, 17);
+            this.radioButtonDollars_euros.TabIndex = 8;
+            this.radioButtonDollars_euros.Text = "Dollars -> Euros";
+            this.radioButtonDollars_euros.UseVisualStyleBackColor = true;
+            this.radioButtonDollars_euros.CheckedChanged += new System.EventHandler(this.radioButtonDollars_euros_CheckedChanged);
+            // 
+            // label_taux
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonDollars_euros);
+            this.Controls.Add(this.radioButtonEuros_dollars);
+            this.Controls.Add(this.label_resultat_devise2);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.radioButton_Dollars);
+            this.Controls.Add(this.label_devise_2);
+            this.Controls.Add(this.label_devise_1);
+            this.Controls.Add(this.textBox_taux);
+            this.Controls.Add(this.label1);
+            this.Name = "label_taux";
+            this.Text = "Devise";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_taux;
+        private System.Windows.Forms.Label label_devise_1;
+        private System.Windows.Forms.Label label_devise_2;
+        private System.Windows.Forms.TextBox radioButton_Dollars;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label_resultat_devise2;
+        private System.Windows.Forms.RadioButton radioButtonEuros_dollars;
+        private System.Windows.Forms.RadioButton radioButtonDollars_euros;
+    }
+}
+
---
+++ TP3 - Copie/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP3
+{
+    public partial class label_taux : Form
+    {
+        public label_taux()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1 (object sender, EventArgs e)
+        {
+
+        }
+        
+
+        /*private void textBox1_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+     {
+         if  (e.KeyChar == (char) 13) button_Click(sender, e);
+     }
+     */
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            int calcul;
+            radioButton_Dollars.Text.Replace(".", ",");
+            textBox_taux.Text.Replace(".", ",");
+            calcul = int.Parse(radioButton_Dollars.Text) * int.Parse(textBox_taux.Text);
+            label_resultat_devise2.Text = (calcul).ToString();
+
+        }
+
+        private void radioButtonEuros_dollars_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox_taux.Text = " ";
+            radioButton_Dollars.Text = " ";
+            label_devise_1.Text = "dollars";
+            label_devise_2.Text = "euros";
+            label_resultat_devise2.Text = " ";
+        }
+
+        private void radioButtonDollars_euros_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox_taux.Text = " ";
+            radioButton_Dollars.Text = " ";
+            label_devise_1.Text = "euros";
+            label_devise_2.Text = "dollars";
+            label_resultat_devise2.Text = " ";
+
+        }
+    }
+}
