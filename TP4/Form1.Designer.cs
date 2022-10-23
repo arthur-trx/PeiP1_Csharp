49eea702c9acd427326268f11a13c503d81458c3
arthur-trx <90566883+arthur-trx@users.noreply.github.com>
2022-10-23 23:10:36 +0200

    Add files via upload

---
+++ TP4/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace TP4
+{
+    partial class Vecteur_v1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opérationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produitScalaireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produitVectorielToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label_v1 = new System.Windows.Forms.Label();
+            this.label_v1_x = new System.Windows.Forms.Label();
+            this.label_v1_y = new System.Windows.Forms.Label();
+            this.label_v1_z = new System.Windows.Forms.Label();
+            this.label_v2 = new System.Windows.Forms.Label();
+            this.label_v3 = new System.Windows.Forms.Label();
+            this.textBox_v1_x = new System.Windows.Forms.TextBox();
+            this.textBox_v1_y = new System.Windows.Forms.TextBox();
+            this.textBox_v1_z = new System.Windows.Forms.TextBox();
+            this.textBox_v2_z = new System.Windows.Forms.TextBox();
+            this.textBox_v2_y = new System.Windows.Forms.TextBox();
+            this.textBox_v2_x = new System.Windows.Forms.TextBox();
+            this.textBox_v3_z = new System.Windows.Forms.TextBox();
+            this.textBox_v3_y = new System.Windows.Forms.TextBox();
+            this.textBox_v3_x = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opérationsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opérationsToolStripMenuItem
+            // 
+            this.opérationsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.produitScalaireToolStripMenuItem,
+            this.produitVectorielToolStripMenuItem});
+            this.opérationsToolStripMenuItem.Name = "opérationsToolStripMenuItem";
+            this.opérationsToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.opérationsToolStripMenuItem.Text = "Opérations";
+            // 
+            // produitScalaireToolStripMenuItem
+            // 
+            this.produitScalaireToolStripMenuItem.Name = "produitScalaireToolStripMenuItem";
+            this.produitScalaireToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.produitScalaireToolStripMenuItem.Text = "produit scalaire";
+            this.produitScalaireToolStripMenuItem.Click += new System.EventHandler(this.produitScalaireToolStripMenuItem_Click);
+            // 
+            // produitVectorielToolStripMenuItem
+            // 
+            this.produitVectorielToolStripMenuItem.Name = "produitVectorielToolStripMenuItem";
+            this.produitVectorielToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.produitVectorielToolStripMenuItem.Text = "produit vectoriel";
+            this.produitVectorielToolStripMenuItem.Click += new System.EventHandler(this.produitVectorielToolStripMenuItem_Click);
+            // 
+            // label_v1
+            // 
+            this.label_v1.AutoSize = true;
+            this.label_v1.Location = new System.Drawing.Point(122, 60);
+            this.label_v1.Name = "label_v1";
+            this.label_v1.Size = new System.Drawing.Size(53, 13);
+            this.label_v1.TabIndex = 1;
+            this.label_v1.Text = "Vecteur 1";
+            // 
+            // label_v1_x
+            // 
+            this.label_v1_x.AutoSize = true;
+            this.label_v1_x.Location = new System.Drawing.Point(93, 89);
+            this.label_v1_x.Name = "label_v1_x";
+            this.label_v1_x.Size = new System.Drawing.Size(23, 13);
+            this.label_v1_x.TabIndex = 2;
+            this.label_v1_x.Text = "X : ";
+            // 
+            // label_v1_y
+            // 
+            this.label_v1_y.AutoSize = true;
+            this.label_v1_y.Location = new System.Drawing.Point(93, 120);
+            this.label_v1_y.Name = "label_v1_y";
+            this.label_v1_y.Size = new System.Drawing.Size(23, 13);
+            this.label_v1_y.TabIndex = 3;
+            this.label_v1_y.Text = "Y : ";
+            // 
+            // label_v1_z
+            // 
+            this.label_v1_z.AutoSize = true;
+            this.label_v1_z.Location = new System.Drawing.Point(93, 150);
+            this.label_v1_z.Name = "label_v1_z";
+            this.label_v1_z.Size = new System.Drawing.Size(23, 13);
+            this.label_v1_z.TabIndex = 4;
+            this.label_v1_z.Text = "Z : ";
+            // 
+            // label_v2
+            // 
+            this.label_v2.AutoSize = true;
+            this.label_v2.Location = new System.Drawing.Point(212, 60);
+            this.label_v2.Name = "label_v2";
+            this.label_v2.Size = new System.Drawing.Size(53, 13);
+            this.label_v2.TabIndex = 5;
+            this.label_v2.Text = "Vecteur 2";
+            // 
+            // label_v3
+            // 
+            this.label_v3.AutoSize = true;
+            this.label_v3.Location = new System.Drawing.Point(308, 60);
+            this.label_v3.Name = "label_v3";
+            this.label_v3.Size = new System.Drawing.Size(41, 13);
+            this.label_v3.TabIndex = 6;
+            this.label_v3.Text = "resultat";
+            // 
+            // textBox_v1_x
+            // 
+            this.textBox_v1_x.Location = new System.Drawing.Point(125, 89);
+            this.textBox_v1_x.Name = "textBox_v1_x";
+            this.textBox_v1_x.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v1_x.TabIndex = 7;
+            // 
+            // textBox_v1_y
+            // 
+            this.textBox_v1_y.Location = new System.Drawing.Point(125, 120);
+            this.textBox_v1_y.Name = "textBox_v1_y";
+            this.textBox_v1_y.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v1_y.TabIndex = 8;
+            // 
+            // textBox_v1_z
+            // 
+            this.textBox_v1_z.Location = new System.Drawing.Point(125, 150);
+            this.textBox_v1_z.Name = "textBox_v1_z";
+            this.textBox_v1_z.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v1_z.TabIndex = 9;
+            // 
+            // textBox_v2_z
+            // 
+            this.textBox_v2_z.Location = new System.Drawing.Point(215, 150);
+            this.textBox_v2_z.Name = "textBox_v2_z";
+            this.textBox_v2_z.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v2_z.TabIndex = 12;
+            // 
+            // textBox_v2_y
+            // 
+            this.textBox_v2_y.Location = new System.Drawing.Point(215, 120);
+            this.textBox_v2_y.Name = "textBox_v2_y";
+            this.textBox_v2_y.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v2_y.TabIndex = 11;
+            // 
+            // textBox_v2_x
+            // 
+            this.textBox_v2_x.Location = new System.Drawing.Point(215, 89);
+            this.textBox_v2_x.Name = "textBox_v2_x";
+            this.textBox_v2_x.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v2_x.TabIndex = 10;
+            // 
+            // textBox_v3_z
+            // 
+            this.textBox_v3_z.Location = new System.Drawing.Point(311, 150);
+            this.textBox_v3_z.Name = "textBox_v3_z";
+            this.textBox_v3_z.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v3_z.TabIndex = 15;
+            // 
+            // textBox_v3_y
+            // 
+            this.textBox_v3_y.Location = new System.Drawing.Point(311, 120);
+            this.textBox_v3_y.Name = "textBox_v3_y";
+            this.textBox_v3_y.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v3_y.TabIndex = 14;
+            // 
+            // textBox_v3_x
+            // 
+            this.textBox_v3_x.Location = new System.Drawing.Point(311, 89);
+            this.textBox_v3_x.Name = "textBox_v3_x";
+            this.textBox_v3_x.Size = new System.Drawing.Size(60, 20);
+            this.textBox_v3_x.TabIndex = 13;
+            // 
+            // Vecteur_v1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_v3_z);
+            this.Controls.Add(this.textBox_v3_y);
+            this.Controls.Add(this.textBox_v3_x);
+            this.Controls.Add(this.textBox_v2_z);
+            this.Controls.Add(this.textBox_v2_y);
+            this.Controls.Add(this.textBox_v2_x);
+            this.Controls.Add(this.textBox_v1_z);
+            this.Controls.Add(this.textBox_v1_y);
+            this.Controls.Add(this.textBox_v1_x);
+            this.Controls.Add(this.label_v2);
+            this.Controls.Add(this.label_v1);
+            this.Controls.Add(this.label_v3);
+            this.Controls.Add(this.label_v1_z);
+            this.Controls.Add(this.label_v1_y);
+            this.Controls.Add(this.label_v1_x);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Vecteur_v1";
+            this.Text = "Vecteur";
+            this.Load += new System.EventHandler(this.Vecteur_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opérationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produitScalaireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produitVectorielToolStripMenuItem;
+        private System.Windows.Forms.Label label_v1;
+        private System.Windows.Forms.Label label_v1_x;
+        private System.Windows.Forms.Label label_v1_y;
+        private System.Windows.Forms.Label label_v1_z;
+        private System.Windows.Forms.Label label_v2;
+        private System.Windows.Forms.Label label_v3;
+        private System.Windows.Forms.TextBox textBox_v1_x;
+        private System.Windows.Forms.TextBox textBox_v1_y;
+        private System.Windows.Forms.TextBox textBox_v1_z;
+        private System.Windows.Forms.TextBox textBox_v2_z;
+        private System.Windows.Forms.TextBox textBox_v2_y;
+        private System.Windows.Forms.TextBox textBox_v2_x;
+        private System.Windows.Forms.TextBox textBox_v3_z;
+        private System.Windows.Forms.TextBox textBox_v3_y;
+        private System.Windows.Forms.TextBox textBox_v3_x;
+    }
+}
+
---
+++ TP4/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP4
+{
+    public partial class Vecteur_v1 : Form
+    {
+        private int[] m_vecteur = new int [3];
+        public Vecteur_v1()
+        {
+            m_vecteur[0] = 0;
+            m_vecteur[1] = 0;
+            m_vecteur[2] = 0;
+            InitializeComponent();
+
+        }
+        public Vecteur_v1(int a, int b, int c)
+        {
+            m_vecteur[0] = a;
+            m_vecteur[1] = b;
+            m_vecteur[2] = c;
+            InitializeComponent();
+
+        }
+        public Vecteur_v1 GetV1()
+        {
+            int a = int.Parse(textBox_v1_x.Text);
+            int b = int.Parse(textBox_v1_y.Text); 
+            int c = int.Parse(textBox_v1_z.Text);
+            Vecteur_v1 V1 = new Vecteur_v1(a,b,c);
+
+            return V1;
+        }
+
+        public Vecteur_v1 GetV2()
+        {
+            int a = int.Parse(textBox_v2_x.Text);
+            int b = int.Parse(textBox_v2_y.Text);
+            int c = int.Parse(textBox_v2_z.Text);
+            Vecteur_v1 V2 = new Vecteur_v1(a, b, c);
+
+            return V2;
+        }
+
+        public Vecteur_v1 Vectoriel (Vecteur_v1 a, Vecteur_v1 b)
+        {
+   
+            int A = a.vecteur_b * b.vecteur_c - a.vecteur_c * b.vecteur_b;
+            int B= a.vecteur_c * b.vecteur_a - a.vecteur_a * b.vecteur_c;
+            int C = a.vecteur_a * b.vecteur_b - a.vecteur_b * b.vecteur_a;
+            Vecteur_v1 vs = new Vecteur_v1(A, B, C);
+            return vs;
+        }
+
+        public int Scalaire (Vecteur_v1 a, Vecteur_v1 b)
+        {
+            int calc = a.vecteur_a * b.vecteur_a + a.vecteur_b * b.vecteur_b + a.vecteur_c * b.vecteur_c;
+
+            return calc;
+        }
+
+        private void Vecteur_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public int vecteur_a
+        {
+            get { return m_vecteur[0]; }
+            set { m_vecteur[0] = value; }
+        }
+        public int vecteur_b
+        {
+            get { return m_vecteur[1]; }
+            set { m_vecteur[1] = value; }
+        }
+        public int vecteur_c
+        {
+            get { return m_vecteur[2]; }
+            set { m_vecteur[2] = value; }
+        }
+
+        /*private void label1_Click(object sender, EventArgs e)
+        {
+
+        }*/
+
+        private void produitScalaireToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         
+            textBox_v3_y.Text = Scalaire(GetV1(), GetV2()).ToString();
+            textBox_v3_x.Visible = false;
+            textBox_v3_z.Visible = false;
+        }
+
+
+
+        private void produitVectorielToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Vecteur_v1 Vsca = new Vecteur_v1();
+            Vsca = Vectoriel(GetV1(), GetV2());
+
+            textBox_v3_x.Text = (Vsca.vecteur_a).ToString();
+            textBox_v3_y.Text = (Vsca.vecteur_b).ToString();
+            textBox_v3_z.Text = (Vsca.vecteur_c).ToString();
+            textBox_v3_x.Visible = true;
+            textBox_v3_z.Visible = true;
+        }
+    }
+}
+/**/
